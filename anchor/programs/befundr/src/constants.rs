e066e675b124f0a8aedb93919a797eb534017496
Jonathan Dugard <9611677+JonathanDugard@users.noreply.github.com>
2024-09-22 10:57:50 +0200

    Add Create project errors and reward errors

--- anchor/programs/befundr/src/constants.rs
+++ anchor/programs/befundr/src/constants.rs
@@ -5,6 +5,22 @@ pub mod user {
     pub const MAX_CITY_LENGTH: usize = 100;
 }
 
+pub mod project {
+    pub const MAX_NAME_LENGTH: u64 = 64; //USD
+    pub const MAX_DESCRIPTION_LENGTH: u64 = 3000; //USD
+    pub const MAX_URL_LENGTH: u64 = 128; //USD
+    pub const MIN_PROJECT_GOAL_AMOUNT: u64 = 0; //USD
+    pub const MIN_PROJECT_CAMPAIGN_DURATION: i64 = 86400; // 1 day
+    pub const MAX_PROJECT_CAMPAIGN_DURATION: i64 = 86400 * 90; //90 days
+    pub const MIN_REWARDS_NUMBER: u16 = 1; //Min of 1 reward
+    pub const MAX_REWARDS_NUMBER: u16 = 10; //Max of 10 rewards
+}
+
+pub mod reward {
+    pub const MAX_NAME_LENGTH: u64 = 64;
+    pub const MAX_DESCRIPTION_LENGTH: u64 = 256;
+}
+
 // Temporary admins keys const
 // FOR LOCAL DEV PURPOSES ONLY
 pub const ADMIN_PUBKEYS: &[&str] = &[
--- anchor/programs/befundr/src/errors.rs
+++ anchor/programs/befundr/src/errors.rs
@@ -9,3 +9,39 @@ pub enum DeleteUserError {
     #[msg("Wrong owner account.")]
     WrongOwnerAccount,
 }
+
+#[error_code]
+pub enum CreateProjectError {
+    #[msg("Project name is too long (max 64 characters).")]
+    NameTooLong,
+    #[msg("Image URL is too long (max 128 characters).")]
+    ImageUrlTooLong,
+    #[msg("Description is too long (max 3000 characters).")]
+    DescriptionTooLong,
+    #[msg("Goal amount is too low (min $1).")]
+    GoalAmountBelowLimit,
+    #[msg("Raised amount cannot exceed the goal amount.")]
+    RaisedAmountExceedsGoal,
+    #[msg("End time must be after the creation time.")]
+    EndTimeBeforeCreatedTime,
+    #[msg("End time beyond the limit")]
+    ExceedingEndTime,
+    #[msg("Not enough rewards (min 1).")]
+    NotEnoughRewards,
+    #[msg("Too many rewards (max 10).")]
+    TooManyRewards,
+}
+
+#[error_code]
+pub enum RewardError {
+    #[msg("Name cannot exceed 64 characters.")]
+    NameTooLong,
+    #[msg("Description cannot exceed 256 characters.")]
+    DescriptionTooLong,
+    #[msg("Price must be greater than 0.")]
+    PriceInvalid,
+    #[msg("Current supply must be greater than 0.")]
+    CurrentSupplyInvalid,
+    #[msg("Max supply must be greater than or equal to current supply.")]
+    MaxSupplyInvalid,
+}
