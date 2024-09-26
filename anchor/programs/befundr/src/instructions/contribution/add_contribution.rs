0f7dbe45657769a56f20f308f59ae4a73475530d
Benjamin Poulinet <bpoulinet@gmail.com>
2024-09-27 01:43:24 +0200

    add contribution without funds transfer

--- anchor/programs/befundr/src/instructions/contribution/add_contribution.rs
+++ anchor/programs/befundr/src/instructions/contribution/add_contribution.rs
@@ -1,69 +1,121 @@
 use crate::{
-    errors::ContributionError,
-    state::{Contribution, Project, Reward, Status, User},
+    errors::{ContributionError, RewardError, TransferError},
+    state::{
+        Contribution, Project, ProjectContributions, ProjectStatus, Reward, User, UserContributions,
+    },
 };
 use anchor_lang::prelude::*;
+use anchor_lang::system_program::{transfer, Transfer};
+// use anchor_spl::token::{self, SplTransfer, Token, TokenAccount};
 
-pub fn add_contribution(ctx: Context<AddContribution>, amount: u64, reward_id: u64) -> Result<()> {
+pub fn add_contribution(
+    ctx: Context<AddContribution>,
+    amount: u64,
+    reward_id: Option<u64>,
+) -> Result<()> {
     let now: i64 = Clock::get()?.unix_timestamp;
     let project = &mut ctx.accounts.project;
     let user = &ctx.accounts.user;
-    let contribution_pda = &mut ctx.accounts.contribution;
+    let user_contributions = &mut ctx.accounts.user_contributions;
+    let contribution = &mut ctx.accounts.contribution;
+    let project_contributions = &mut ctx.accounts.project_contributions;
 
     // Check if the project is in a fundraising state and in fundraising period
-    require!(project.status == Status::Fundraising, ContributionError::ProjectNotFundraising);
+    require!(
+        project.status == ProjectStatus::Fundraising,
+        ContributionError::ProjectNotFundraising
+    );
     require!(project.end_time > now, ContributionError::ProjectNotFundraising);
 
-    // 2. Verify that the signer is the actual user pda owner
+    // Verify that the signer is the actual user pda owner
     require!(user.owner == ctx.accounts.signer.key(), ContributionError::SignerNotUser);
 
-    // We need to ensure the selected reward exists in the project rewards list
-    // DEV NOTES: it's not the best way to check if a selected reward
-    // is the right one from a project, a model update will be needed
-    // to be more strict with rewards uid instead of an vector index.
-    let reward: &Reward = project
-        .rewards
-        .get(reward_id as usize)
-        .ok_or(ContributionError::RewardError)?;
+    // Amount must be positive and greater than 0
+    require!(amount > 0, ContributionError::RewardAlreadyReserved);
 
-    // Validate that the contribution amount is sufficient for the selected reward
-    require!(amount >= reward.price, ContributionError::RewardPriceError);
-
-    // Check if the Contribution PDA already exists
-    require!(contribution_pda.amount == 0, ContributionError::RewardAlreadyReserved);
-
-    // Transfer the contribution amount in lamports to the project
-
-    // TODO: handle USDC ATA
-    **project.to_account_info().try_borrow_mut_lamports()? += amount;
-    **user.to_account_info().try_borrow_mut_lamports()? -= amount;
-
-    /*
-     *  Data updates
-     */
+    let mut reward: Option<&mut Reward> = None;
+    if reward_id.is_some() {
+        reward_validation(project, amount, reward_id.unwrap())?;
+        // set a mutable variable with project reward
+        reward = project.rewards.get_mut(reward_id.unwrap() as usize);
+    }
+    // Update reward supply
+    if let Some(reward) = reward {
+        reward.add_supply()?;
+    }
 
     // Initialize the Contribution PDA if all checks pass
-    contribution_pda.initial_owner = user.key();
-    contribution_pda.current_owner = user.key();
-    contribution_pda.amount = amount;
-    contribution_pda.reward_id = reward_id;
-    contribution_pda.timestamp = now;
-    contribution_pda.is_for_sale = false;
-    contribution_pda.selling_price = None;
+    contribution.initial_owner = user.key();
+    contribution.current_owner = user.key();
+    contribution.project = project.key();
+    contribution.amount = amount;
+    contribution.reward_id = reward_id;
+    contribution.creation_timestamp = now;
+    contribution.is_for_sale = false;
+    contribution.selling_price = None;
+    contribution.set_active();
 
-    // Update the project's raised_amount with the new contribution
-    // Should be named with usdc prefix: usdc_raised_amount
-    // Todo: handle USDC
+    // Update project's datas
     project.raised_amount += amount;
-
-    // Update the project's contribution_counter and the user's contribution amount
     project.contribution_counter += 1;
 
-    // Update project reward current supply
-    if let Some(reward) = project.rewards.get_mut(reward_id as usize) {
-        reward.current_supply += 1;
-    } else {
-        return Err(ContributionError::RewardError.into());
+    // Update ProjectContributions List
+    project_contributions.contributions.push(contribution.key());
+
+    // Update user contributions
+    user_contributions.contributions.push(contribution.key());
+
+    // Transfer the contribution amount in USDC to the project
+    // transfer_lamports_funds(&ctx, amount);
+    // transfer_spl_funds(&ctx, amount);
+
+    Ok(())
+}
+
+fn transfer_lamports_funds(ctx: &Context<AddContribution>, amount: u64) -> Result<()> {
+    let transfer_instruction = Transfer {
+        from: ctx.accounts.signer.to_account_info(),
+        to: ctx.accounts.project.to_account_info(),
+    };
+    let cpi_ctx =
+        CpiContext::new(ctx.accounts.system_program.to_account_info(), transfer_instruction);
+
+    match transfer(cpi_ctx, amount.into()) {
+        Ok(_) => Ok(()),
+        Err(error) => {
+            msg!("Error during the contribution transfer: {:?}", error);
+            Err(TransferError::TransferFailed.into())
+        },
+    }
+}
+
+// fn transfer_spl_tokens(ctx: Context<AddContribution>, amount: u64) -> Result<()> {
+//     let destination = &ctx.accounts.to_ata;
+//     let source = &ctx.accounts.from_ata;
+//     let token_program = &ctx.accounts.token_program;
+//     let authority = &ctx.accounts.from;
+
+//     // Transfer tokens from taker to initializer
+//     let cpi_accounts = SplTransfer {
+//         from: source.to_account_info().clone(),
+//         to: destination.to_account_info().clone(),
+//         authority: authority.to_account_info().clone(),
+//     };
+//     let cpi_program = token_program.to_account_info();
+
+//     token::transfer(CpiContext::new(cpi_program, cpi_accounts), amount)?;
+//     Ok(())
+// }
+
+pub fn reward_validation(project: &Project, amount: u64, reward_id: u64) -> Result<()> {
+    // We need to ensure the selected reward exists in the project rewards list
+    if let Some(reward) = project.rewards.get(reward_id as usize) {
+        // Validate that the contribution amount is sufficient for the selected reward
+        require!(amount >= reward.price, ContributionError::RewardPriceError);
+        // Validate and update the reward supply
+        if let Some(max_supply) = reward.max_supply {
+            require!(reward.current_supply < max_supply as u32, RewardError::RewardSupplyReach);
+        }
     }
 
     Ok(())
@@ -75,10 +127,15 @@ pub struct AddContribution<'info> {
     pub project: Account<'info, Project>,
 
     #[account(mut)]
+    pub project_contributions: Account<'info, ProjectContributions>,
+
     pub user: Account<'info, User>,
 
+    #[account(mut)]
+    pub user_contributions: Account<'info, UserContributions>,
+
     #[account(
-        init_if_needed,
+        init,
         payer = signer,
         space = 8 + Contribution::INIT_SPACE,
         seeds = [b"contribution", project.key().as_ref(), &(project.contribution_counter + 1).to_le_bytes()],
