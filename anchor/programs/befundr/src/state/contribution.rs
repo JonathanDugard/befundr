581d560edc30ec73ae01a5b3f1c108fd032097b7
Benjamin Poulinet <bpoulinet@gmail.com>
2024-09-26 22:59:24 +0200

    Update contribution struct + status enum

--- anchor/programs/befundr/src/state/contribution.rs
+++ anchor/programs/befundr/src/state/contribution.rs
@@ -6,9 +6,17 @@ pub struct Contribution {
     //Should contain Signer Pubkey
     pub initial_owner: Pubkey, // User PDA pubkey
     pub current_owner: Pubkey, // User PDA pubkey
+    pub project: Pubkey,
     pub amount: u64,
-    pub reward_id: u64,
-    pub timestamp: i64,
+    pub reward_id: Option<u64>,
+    pub creation_timestamp: i64,
     pub is_for_sale: bool,
     pub selling_price: Option<u64>,
+    pub status: Status,
+}
+
+#[derive(Clone, InitSpace, AnchorSerialize, AnchorDeserialize, PartialEq, Eq)]
+pub enum Status {
+    Active,
+    Cancelled,
 }
