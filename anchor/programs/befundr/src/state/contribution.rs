0f72391e71512bccbeaaed8dc8ec7ba38dfb0245
Benjamin Poulinet <bpoulinet@gmail.com>
2024-09-27 01:24:47 +0200

    add contribution status methods

--- anchor/programs/befundr/src/state/contribution.rs
+++ anchor/programs/befundr/src/state/contribution.rs
@@ -1,5 +1,11 @@
 use anchor_lang::prelude::*;
 
+#[derive(Copy, Clone, InitSpace, AnchorSerialize, AnchorDeserialize, PartialEq, Eq)]
+pub enum ContributionStatus {
+    Active,
+    Cancelled,
+}
+
 #[account]
 #[derive(InitSpace)]
 pub struct Contribution {
@@ -12,11 +18,39 @@ pub struct Contribution {
     pub creation_timestamp: i64,
     pub is_for_sale: bool,
     pub selling_price: Option<u64>,
-    pub status: ContributionStatus,
+    status: ContributionStatus,
 }
 
-#[derive(Clone, InitSpace, AnchorSerialize, AnchorDeserialize, PartialEq, Eq)]
-pub enum ContributionStatus {
-    Active,
-    Cancelled,
+impl Default for Contribution {
+    fn default() -> Self {
+        Self {
+            initial_owner: Pubkey::default(),
+            current_owner: Pubkey::default(),
+            project: Pubkey::default(),
+            amount: 0,
+            reward_id: None,
+            creation_timestamp: 0,
+            is_for_sale: false,
+            selling_price: None,
+            status: ContributionStatus::Active,
+        }
+    }
+}
+
+impl Contribution {
+    pub fn get_status(&self) -> ContributionStatus {
+        self.status
+    }
+
+    pub fn is_active(&self) -> bool {
+        self.status == ContributionStatus::Active
+    }
+
+    pub fn set_active(&mut self) {
+        self.status = ContributionStatus::Active;
+    }
+
+    pub fn set_cancelled(&mut self) {
+        self.status = ContributionStatus::Cancelled;
+    }
 }
