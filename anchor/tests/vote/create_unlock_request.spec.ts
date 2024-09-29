5db9bc38bc59085bd89a79a4657ccb6d02c2c4d0
Jonathan Dugard <9611677+JonathanDugard@users.noreply.github.com>
2024-09-29 11:15:28 +0200

    Prepare unit tests

---
+++ anchor/tests/vote/create_unlock_request.spec.ts
@@ -0,0 +1,11 @@
+import { program } from "../config";
+import { createProject, createUser, createUserWalletWithSol } from "../utils";
+import { ONE_DAY_MILLISECONDS, projectData1 } from "../project/project_dataset";
+import { userData1 } from "../user/user_dataset";
+import { BN } from "@coral-xyz/anchor";
+
+describe('createUnlockVote', () => {
+    it("should successfully create an unlock vote", async () => {
+        //TODO Wait for the contribution to be implemented
+    });
+});
