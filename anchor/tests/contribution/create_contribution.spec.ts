8079f152865004c18b628d428b397312e3cb8d9e
Benjamin Poulinet <bpoulinet@gmail.com>
2024-09-30 11:00:52 +0200

    wip tests update

--- anchor/tests/contribution/create_contribution.spec.ts
+++ anchor/tests/contribution/create_contribution.spec.ts
@@ -3,7 +3,7 @@ import { createContribution, createProject, createUser, createUserWalletWithSol 
 import { ONE_DAY_MILLISECONDS, projectData1 } from "../project/project_dataset";
 import { userData1, userData2} from "../user/user_dataset";
 import { BN } from "@coral-xyz/anchor";
-import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { LAMPORTS_PER_SOL, Enum } from "@solana/web3.js";
 import { ContributionStatus } from "./contribution_status";
 
 describe('createContribution', () => {
@@ -38,7 +38,84 @@ describe('createContribution', () => {
         expect(contributionPda.rewardId.toNumber()).toEqual(0);
         expect(contributionPda.creationTimestamp.toNumber()).toBeGreaterThan(0);
         expect(contributionPda.isClaimed).toBeNull();
-        //expect(contributionPda.status).toEqual(ContributionStatus.Active);
-
+        expect(new Enum(contributionPda.status).enum).toBe(ContributionStatus.Active.enum);
     });
+
+    it("should fail if the project is not in fundraising state", async () => {
+        // no project state updates instruction exist at this time
+    });
+
+    it("should fail if the project is not in fundraising period", async () => {
+        // unable to create a past project 
+    });
+
+    it("should fail if the signer is not the actual user PDA owner", async () => {
+        const creatorWallet = await createUserWalletWithSol();
+        const creatorUserPdaKey = await createUser(userData1, creatorWallet);
+        const projectPdaKey = await createProject(projectData1, 0, creatorUserPdaKey, creatorWallet);
+
+        const userWallet = await createUserWalletWithSol();
+        const userPdaKey = await createUser(userData2, userWallet);
+
+        const projectPda = await program.account.project.fetch(projectPdaKey);
+        const projectContributionCounter = projectPda.contributionCounter;
+        const contributionAmount = 1 * LAMPORTS_PER_SOL;
+
+        // Create a different wallet to simulate the wrong signer
+        const wrongWallet = await createUserWalletWithSol();
+
+        const expectedErrorMessage = new RegExp("Signer must be the user.");
+
+        await expect(
+            createContribution(
+                projectPdaKey,
+                userPdaKey,
+                wrongWallet, // Using the wrong wallet here
+                projectContributionCounter,
+                contributionAmount,
+                0
+            )
+        ).rejects.toThrow(expectedErrorMessage);
+    });
+
+    it("should fail if the contribution amount is not positive and greater than 0", async () => {
+        // empty
+    });
+
+    it("should fail if the reward does not exist in the project rewards list", async () => {
+        // empty
+    });
+
+    it("should fail if the contribution amount is insufficient for the selected reward", async () => {
+        // empty
+    });
+
+    it("should fail if the reward supply has reached its maximum limit", async () => {
+        // empty
+    });
+
+    it("should update the reward supply if a valid reward is selected", async () => {
+        // empty
+    });
+
+    it("should update the project's raised amount and contribution counter", async () => {
+        // empty
+    });
+
+    it("should update the ProjectContributions list", async () => {
+        // empty
+    });
+
+    it("should update the UserContributions list", async () => {
+        // empty
+    });
+
+    it("should transfer the contribution amount in USDC to the project", async () => {
+        // empty
+    });
+
+    it("should handle errors during the contribution transfer", async () => {
+        // empty
+    });
+
 });
