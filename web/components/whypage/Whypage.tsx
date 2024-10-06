51816b432eac3743d21d9f90a059db502e2a3559
Benjamin Poulinet <bpoulinet@gmail.com>
2024-10-06 10:23:26 +0200

    why befundr page

--- web/components/whypage/Whypage.tsx
+++ web/components/whypage/Whypage.tsx
@@ -6,68 +6,57 @@ type Props = {};
 const Whypage = (props: Props) => {
   return (
     <div className="flex flex-col items-start justify-start gap-10 w-full">
-      <h1 className="textStyle-title">Why beFundr ?</h1>
+      <h1 className="textStyle-title">Why <strong className="!text-accent">beFundr</strong>?</h1>
+      
       <h2 className="textStyle-headline -mt-10">
-        Depending on your role on the platform, discover why beFundr make the
-        crowdfunding better
+        Depending on your role on the platform, discover how <strong className="!text-accent">beFundr</strong> makes crowdfunding better.
       </h2>
+      
       <h3 className="textStyle-subtitle -mb-10">
-        As <strong className="!text-accent">founder, ean more</strong> if you
-        drive well your project{' '}
+        As a <strong className="!text-accent">Founder</strong>, earn more if you deliver.
       </h3>
-      <Divider />
-      <p className="textStyle-body mb-10">
-        Sed feugiat. Cum sociis natoque penatibus et magnis dis parturient
-        montes, nascetur ridiculus mus. Ut pellentesque augue sed urna.
-        Vestibulum diam eros, fringilla et, consectetuer eu, nonummy id, sapien.
-        Nullam at lectus. In sagittis ultrices mauris. Curabitur malesuada erat
-        sit amet massa. Fusce blandit. Aliquam erat volutpat. Aliquam euismod.
-        Aenean vel lectus. Nunc imperdiet justo nec dolor. <br />
-        Nam quis ante. Nullam interdum quam in eros. Sed eleifend libero eu
-        tellus consequat fermentum. Nullam pellentesque risus ut augue.
-        Vestibulum eu tellus. Integer eleifend suscipit urna. Fusce porttitor
-        leo et odio. Vivamus vehicula justo a nisl. In rutrum, purus ut dictum
-        auctor, dolor velit accumsan dolor, eu convallis augue dui ac lectus.
-        Nullam eleifend pellentesque ligula. Nam quis magna. Donec elementum
-        dapibus erat. Pellentesque vel ipsum nec orci fermentum accumsan. Nunc
-        porta magna eu neque. Nam id erat eu mi aliquet cursus. Morbi ut felis.
-        Vestibulum in ipsum. <br />
-        Integer ac diam. Nullam porttitor dolor eget metus. Nulla sed metus quis
-        tortor lacinia tempor. Mauris mauris dui, faucibus vitae, aliquet sit
-        amet, placerat a, ante. Nunc placerat tincidunt neque. Mauris egestas
-        dolor ut ipsum cursus malesuada. Curabitur odio. Nunc lobortis. Sed
-        mattis tempor felis. Mauris dolor quam, facilisis at, bibendum sit amet,
-        rutrum ornare, pede. Suspendisse accumsan sagittis velit. Pellentesque
-        varius laoreet lorem. Vivamus egestas sapien id diam.
+      
+      <p className="textStyle-body mb-2">
+        At <strong className="!text-accent">beFundr</strong>, we know that your project is more than just an idea—it's a journey.
       </p>
-      <h3 className="textStyle-subtitle -mb-10">
-        As <strong className="!text-accent">contributor</strong> invest in{' '}
-        <strong className="!text-accent">security</strong>
-      </h3>
+      <p className="textStyle-body mx--2">See what beFundr brings to you:</p>
+      <ul className="list-disc pl-5 mb-4">
+        <li className="textStyle-body">
+          <strong className="!text-accent">Drive your success:</strong> Gain access to a transparent, community-driven funding system that rewards you for delivering on your project milestones.
+        </li>
+        <li className="textStyle-body">
+          <strong className="!text-accent">Trust at the core:</strong> Build trust through milestone-based funding, giving backers confidence and ensuring you stay focused on your goals.
+        </li>
+        <li className="textStyle-body">
+          <strong className="!text-accent">Keep more:</strong> Lower fees compared to traditional crowdfunding means more funds go directly to your project.
+        </li>
+      </ul>
+      
       <Divider />
+      
+      <h3 className="textStyle-subtitle -mb-10">
+        As a <strong className="!text-accent">Contributor</strong>, invest in <strong className="!text-accent">security</strong>.
+      </h3>
+      
+      <p className="textStyle-body mb-10">
+        Supporting a project should feel as rewarding as delivering one. On <strong className="!text-accent">beFundr</strong>:
+      </p>
+      <ul className="list-disc pl-5 mb-10">
+        <li className="textStyle-body">
+          <strong className="!text-accent">Protect your contribution:</strong> With blockchain, your funds are locked and secure until the project hits key milestones.
+        </li>
+        <li className="textStyle-body">
+          <strong className="!text-accent">Real-time transparency:</strong> Always know how your funds are being used and where the project stands.
+        </li>
+        <li className="textStyle-body">
+          <strong className="!text-accent">Contribute with confidence:</strong> If a project fails to deliver, funds are returned, protecting your investment.
+        </li>
+      </ul>
+      
+      <Divider />
+      
       <p className="textStyle-body">
-        Sed feugiat. Cum sociis natoque penatibus et magnis dis parturient
-        montes, nascetur ridiculus mus. Ut pellentesque augue sed urna.
-        Vestibulum diam eros, fringilla et, consectetuer eu, nonummy id, sapien.
-        Nullam at lectus. In sagittis ultrices mauris. Curabitur malesuada erat
-        sit amet massa. Fusce blandit. Aliquam erat volutpat. Aliquam euismod.
-        Aenean vel lectus. Nunc imperdiet justo nec dolor. <br />
-        Nam quis ante. Nullam interdum quam in eros. Sed eleifend libero eu
-        tellus consequat fermentum. Nullam pellentesque risus ut augue.
-        Vestibulum eu tellus. Integer eleifend suscipit urna. Fusce porttitor
-        leo et odio. Vivamus vehicula justo a nisl. In rutrum, purus ut dictum
-        auctor, dolor velit accumsan dolor, eu convallis augue dui ac lectus.
-        Nullam eleifend pellentesque ligula. Nam quis magna. Donec elementum
-        dapibus erat. Pellentesque vel ipsum nec orci fermentum accumsan. Nunc
-        porta magna eu neque. Nam id erat eu mi aliquet cursus. Morbi ut felis.
-        Vestibulum in ipsum. <br />
-        Integer ac diam. Nullam porttitor dolor eget metus. Nulla sed metus quis
-        tortor lacinia tempor. Mauris mauris dui, faucibus vitae, aliquet sit
-        amet, placerat a, ante. Nunc placerat tincidunt neque. Mauris egestas
-        dolor ut ipsum cursus malesuada. Curabitur odio. Nunc lobortis. Sed
-        mattis tempor felis. Mauris dolor quam, facilisis at, bibendum sit amet,
-        rutrum ornare, pede. Suspendisse accumsan sagittis velit. Pellentesque
-        varius laoreet lorem. Vivamus egestas sapien id diam.
+        Join the future of crowdfunding. At <strong className="!text-accent">beFundr</strong>, we’re here to make crowdfunding transparent, secure, and rewarding for everyone. Start your journey with us today!
       </p>
     </div>
   );
