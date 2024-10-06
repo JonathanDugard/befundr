1bfe631d75ae155ba22b1cafc338d135758dc5f8
Benjamin Poulinet <bpoulinet@gmail.com>
2024-10-06 10:48:08 +0200

    1/n whypage update text and margins

--- web/components/whypage/Whypage.tsx
+++ web/components/whypage/Whypage.tsx
@@ -5,21 +5,21 @@ type Props = {};
 
 const Whypage = (props: Props) => {
   return (
-    <div className="flex flex-col items-start justify-start gap-10 w-full">
+    <div className="flex flex-col items-start justify-start gap-4 w-full">
       <h1 className="textStyle-title">Why <strong className="!text-accent">beFundr</strong>?</h1>
       
-      <h2 className="textStyle-headline -mt-10">
+      <h2 className="textStyle-body mb-4">
         Depending on your role on the platform, discover how <strong className="!text-accent">beFundr</strong> makes crowdfunding better.
       </h2>
-      
-      <h3 className="textStyle-subtitle -mb-10">
+      <Divider />
+      <h3 className="textStyle-subtitle">
         As a <strong className="!text-accent">Founder</strong>, earn more if you deliver.
       </h3>
       
-      <p className="textStyle-body mb-2">
+      <p className="textStyle-body">
         At <strong className="!text-accent">beFundr</strong>, we know that your project is more than just an idea—it's a journey.
       </p>
-      <p className="textStyle-body mx--2">See what beFundr brings to you:</p>
+      <p className="textStyle-body">See what beFundr brings to you:</p>
       <ul className="list-disc pl-5 mb-4">
         <li className="textStyle-body">
           <strong className="!text-accent">Community driven success:</strong> Gain access to a transparent, community-driven funding system promoting trust and long-term success for your projects.
@@ -34,11 +34,11 @@ const Whypage = (props: Props) => {
       
       <Divider />
       
-      <h3 className="textStyle-subtitle -mb-10">
+      <h3 className="textStyle-subtitle">
         As a <strong className="!text-accent">Contributor</strong>, invest in <strong className="!text-accent">security</strong>.
       </h3>
       
-      <p className="textStyle-body mb-10">
+      <p className="textStyle-body">
         Supporting a project should feel as rewarding as delivering one. On <strong className="!text-accent">beFundr</strong>:
       </p>
       <ul className="list-disc pl-5 mb-10">
@@ -52,9 +52,6 @@ const Whypage = (props: Props) => {
           <strong className="!text-accent">Contribute with confidence:</strong> If a project fails to deliver, funds are returned, protecting your investment.
         </li>
       </ul>
-      
-      <Divider />
-      
       <p className="textStyle-body">
         Join the future of crowdfunding. At <strong className="!text-accent">beFundr</strong>, we’re here to make crowdfunding transparent, secure, and rewarding for everyone. Start your journey with us today!
       </p>
