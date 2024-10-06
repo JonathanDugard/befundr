1e5aa742d72e70db7a026e6f9f07b7e56dc2b4c6
Benjamin Poulinet <bpoulinet@gmail.com>
2024-10-06 10:22:41 +0200

    show & hide InfoLabel options

--- web/components/z-library/display_elements/InfoLabel.tsx
+++ web/components/z-library/display_elements/InfoLabel.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 type Props = {
   label: string;
+  faeyeicon?: boolean;
+  faeyeslashicon?: boolean;
 };
 
 const InfoLabel = (props: Props) => {
   return (
     <div
       className="
-      bg-none rounded-md h-8 min-w-44 px-2 py-1 border border-accent
-        textStyle-body-accent text-center
+      rounded-md h-8 min-w-44 px-3 py-1 border border-gray-300
+      textStyle-body-accent text-center shadow-sm hover:bg-gray-200
+      flex items-center justify-center space-x-2 font-weight:600
     "
     >
-      {props.label}
+      {props.faeyeicon && <FaEye />}
+      {props.faeyeslashicon && <FaEyeSlash />}
+      <span>{props.label}</span>
     </div>
   );
 };
