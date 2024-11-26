9ddf29f61f24a449b102fcbd687b5822b04a2338
Jonathan Dugard <9611677+JonathanDugard@users.noreply.github.com>
2024-11-26 17:02:56 +0100

    Fix unlock request seeds

--- anchor/programs/befundr/src/instructions/vote/claim_unlock_request.rs
+++ anchor/programs/befundr/src/instructions/vote/claim_unlock_request.rs
@@ -62,18 +62,15 @@ pub fn claim_unlock_request(
 }
 
 #[derive(Accounts)]
-
+#[instruction(created_project_counter: u8)]
 pub struct ClaimUnlockRequest<'info> {
     #[account(has_one = owner)]
     pub user: Account<'info, User>,
 
-    #[account(has_one = project,
-    seeds = [b"project_unlock_requests", project.key().as_ref()],
-    bump
-  )]
+    #[account(has_one = project, seeds = [b"project_unlock_requests", project.key().as_ref()], bump)]
     pub unlock_requests: Account<'info, UnlockRequests>,
 
-    #[account(mut,has_one = project)]
+    #[account(mut, has_one = project)]
     pub current_unlock_request: Account<'info, UnlockRequest>,
 
     #[account(mut)]
@@ -82,8 +79,7 @@ pub struct ClaimUnlockRequest<'info> {
     #[account(mut)]
     pub to_ata: Account<'info, TokenAccount>,
 
-    #[account(has_one = user, seeds = [b"project", user.key().as_ref(), &(user.created_project_counter + 0).to_le_bytes()],
-    bump)]
+    #[account(has_one = user, seeds = [b"project", user.key().as_ref(), &(created_project_counter + 0).to_le_bytes()], bump)]
     pub project: Account<'info, Project>,
 
     #[account(mut)]
